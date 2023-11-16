ab07f585e4d9c2a7933138b50110e8c956b766ef
Aashay Pawar <114884189+pawar-aa@users.noreply.github.com>
2023-11-16 17:23:16 -0500

    Asg9

---
+++ Assignment 9/models/user.js
@@ -0,0 +1,26 @@
+// models/user.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre('save', async function (next) {
+  // Hash the password before saving to the database
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+    } catch (error) {
+      throw error;
+    }
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
